c9881a1d8ec5416a3ee91d6980fc545956fe2cd4
Igor M <terra.kot.ig@gmail.com>
2022-03-28 23:12:56 +0300

    feat: stack solution

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Implement the Stack with a given interface via array.
  *
@@ -13,19 +11,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this._stack = [];
   }
-
+  push( element ) {
+    this._stack.push(element);
+    return this;
+  }
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._stack.pop();
   }
-
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._stack.slice(-1)[0];
   }
 }
 
